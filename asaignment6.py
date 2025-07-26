40c9dbc16a9c73a0db0c9341e93b06435da30486
siam2330095 <mdjahidhossensiam@gmail.com>
2025-07-26 07:48:18 +0600

    Add files via upload

---
+++ asaignment6.py
@@ -0,0 +1,64 @@
+# from collections import deque
+
+# def has_path(grid):
+#     if not grid or grid[0][0] == 0:
+#         return False
+    
+#     m, n = len(grid), len(grid[0])
+#     visited = [[False]*n for _ in range(m)]
+#     directions = [(-1,0), (1,0), (0,-1), (0,1)]  # Up, Down, Left, Right
+    
+#     queue = deque()
+#     queue.append((0, 0))
+#     visited[0][0] = True
+
+#     while queue:
+#         x, y = queue.popleft()
+        
+#         if x == m-1 and y == n-1:
+#             return True  # Reached bottom-right
+        
+#         for dx, dy in directions:
+#             nx, ny = x + dx, y + dy
+            
+#             if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1 and not visited[nx][ny]:
+#                 visited[nx][ny] = True
+#                 queue.append((nx, ny))
+    
+#     return False
+# grid = [
+#     [1,0,1],
+#     [1,1,1],
+#     [0,0,1]
+# ]
+
+# print(has_path(grid)) 
+from collections import deque
+def has_path(grid):
+    if not grid and grid[0][0]==0:
+        return False
+    m,n=len(grid),len(grid[0])
+    visited=[[False]*n for _ in range(m)]
+    directions = [(-1,0), (1,0), (0,-1), (0,1)] 
+    queue=deque()
+    queue.append((0,0))
+    visited[0][0]=True
+    while queue:
+        x,y=queue.popleft()
+        if x==m-1 and y==n-1:
+            return True #buttom reached
+        
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0<=nx<m and 0<=ny<n and grid[nx][ny]==1 and not visited[nx][ny]:
+                visited[nx][ny]=True
+                queue.append((nx,ny))
+    return False 
+grid = [
+    [1,0,1],
+    [1,1,1],
+    [0,0,1]
+]
+
+print(has_path(grid)) 
+    
